539587185cc29d2814fec3af584b22910c9a7d43
AnudeepVemula177 <anudheepvemula@gmail.com>
2026-09-18 16:39:55 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='studenthub-v14-v1';
+const CORE=['./','./index.html','./site.webmanifest','./about.html','./contact.html','./privacy.html','./terms.html','./study-tools.html'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
